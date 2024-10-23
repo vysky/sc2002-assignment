febf3b417f735ba78b832dd70ce6f90476e2cbbe
Justin <justinlimweiyang@gmail.com>
2024-10-23 19:24:47 +0800

    Fixed bug with project execution

--- src/main/java/hms/user/helpers/ExcelReader.java
+++ src/main/java/hms/user/helpers/ExcelReader.java
@@ -1,11 +1,14 @@
 package hms.user.helpers;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelReader {
     public static List<List<Object>> readPatientData(String filePath) {
@@ -30,6 +33,6 @@ public class ExcelReader {
             e.printStackTrace();
         }
 
-        return data;
+        return patients;
     }
 }
