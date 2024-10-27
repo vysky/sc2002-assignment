07555bdd212610a4dfb6ef481e7c6b1dfbbf1e47
Justin <justinlimweiyang@gmail.com>
2024-10-27 15:14:10 +0800

    Created staff.java (WIP)

---
+++ src/main/java/hms/user/Staff.java
@@ -0,0 +1,45 @@
+package hms.user;
+
+public class Staff extends User {
+    
+    private String staffId;
+    private String name;
+    private String role;
+    private String gender;
+    private int age;
+
+    public Staff(String staffId, String name, String role, String gender, int age) {
+        super(staffId, "password", true);
+        this.staffId = staffId;
+        this.name = name;
+        this.role = role;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    // GETTERS
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // SETTERS
+
+    
+}
