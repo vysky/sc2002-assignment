a86b361a5290cebfc77441b84534cb8d1a0204eb
WeiPang <167313036+C230071@users.noreply.github.com>
2024-11-18 13:42:33 +0800

    Merge from main and update

--- src/main/java/hms/appointment/AppointmentManager.java
+++ src/main/java/hms/appointment/AppointmentManager.java
@@ -2,6 +2,7 @@ package hms.appointment;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.FileReader;
@@ -11,9 +12,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppointmentManager {
+    private static final String STAFF_FILE = "src\\main\\resources\\staff.csv";
     private static final String AVAILABILITY_FILE = "src\\main\\resources\\Timeslot.csv";
     private static final String APPOINTMENTS_FILE = "src\\main\\resources\\Appointment_Record.csv";
 
+    private List<Appointment> appointments;
+    //private List<Doctor> doctors;
+
+
+
+    public List<String[]> listAllDoctors() {
+        List<String[]> doctors = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(STAFF_FILE))) {
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                if ("doctor".equalsIgnoreCase(nextLine[2])) {
+                    doctors.add(nextLine);
+                }
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+        return doctors;
+    }
+    
     public List<String> getAvailableTimeslots(String doctorId, String date) {
 
         List<String> availableTimeslots = new ArrayList<>();
@@ -33,7 +57,7 @@ public class AppointmentManager {
         return availableTimeslots;
     }
 
-     public boolean isAvailable(String doctorId, String date, String timeslot) {
+    public boolean isAvailable(String doctorId, String date, String timeslot) {
 
         try (CSVReader reader = new CSVReader(new FileReader(AVAILABILITY_FILE))) {
 
@@ -52,11 +76,54 @@ public class AppointmentManager {
         return false;
     }
 
-    public void makeAppointment(String doctorId, String date, String timeslot) {
+    public String[] hasExistingAppointment(String patientId, String doctorId) {
+        try (CSVReader reader = new CSVReader(new FileReader(APPOINTMENTS_FILE))) {
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                if (nextLine[1].equals(patientId) && nextLine[2].equals(doctorId) &&
+                        (nextLine[5].equals("Pending") || nextLine[5].equals("Confirmed"))) {
+                    return new String[]{nextLine[3], nextLine[4]};
+                }
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String generateAppointmentID() {
+        String lastAppointmentID = getLastAppointmentID();
+        if (lastAppointmentID == null) {
+            return "A00001";
+        }
+
+        int numericPart = Integer.parseInt(lastAppointmentID.substring(1));
+        int newNumericPart = numericPart + 1;
+        return String.format("A%05d", newNumericPart);
+    }
+
+    private String getLastAppointmentID() {
+        String lastAppointmentID = null;
+
+        try (CSVReader reader = new CSVReader(new FileReader(APPOINTMENTS_FILE))) {
+            List<String[]> allRecords = reader.readAll();
+            if (!allRecords.isEmpty()) {
+                lastAppointmentID = allRecords.get(allRecords.size() - 1)[0];
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+
+        return lastAppointmentID;
+    }
+
+    public void makeAppointment(String patientId, String doctorId, String date, String timeslot) {
 
         updateAvailability(doctorId, date, timeslot, "Not Available");
+        String appointmentID = generateAppointmentID();
+        String appointmentStatus = "Pending";
 
-        String[] appointment = {doctorId, date, timeslot};
+        String[] appointment = {appointmentID, patientId, doctorId, date, timeslot, appointmentStatus};
 
         try (CSVWriter appointmentWriter = new CSVWriter(new FileWriter(APPOINTMENTS_FILE, true))) {
             appointmentWriter.writeNext(appointment);
@@ -64,7 +131,7 @@ public class AppointmentManager {
             e.printStackTrace();
         }
 
-        System.out.println("Appointment made successfully.");
+        System.out.println("Appointment made successfully. Your appointment ID is " + appointmentID);
     }
 
     public void updateAvailability(String doctorId, String date, String timeslot, String availability) {
