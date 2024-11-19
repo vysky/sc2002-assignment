011b376fe7b85b0bd8595963058ba2a00d8ab9c0
wteo026 <167312881+wteo026@users.noreply.github.com>
2024-11-19 17:51:00 +0800

    Update AppointmentRepository.java

--- src/main/java/hms/repository/AppointmentRepository.java
+++ src/main/java/hms/repository/AppointmentRepository.java
@@ -9,10 +9,17 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The AppointmentRepository class provides methods to import and export appointment data
+ * from and to a CSV file. It implements the CsvRepository interface for handling appointment objects.
+ */
+
 public class AppointmentRepository implements CsvRepository<Appointment>
 {
+     // Path to the CSV file where appointments are stored
     static final String CSV_FILE_PATH_APPOINTMENT = "src/main/resources/appointment.csv";
-
+    
+    // Headers for the CSV file
     String[] APPOINTMENT_HEADERS = {"Appointment ID", "Patient", "Doctor", "Date", "Timeslot", "Appointment_Status", "Service", "Medication", "Notes", "Prescription_Status"};
 
     /**
@@ -26,9 +33,11 @@ public class AppointmentRepository implements CsvRepository<Appointment>
 
         try
         {
+            // Create a reader for the CSV file
             Reader reader = new FileReader(CSV_FILE_PATH_APPOINTMENT);
             Iterable<CSVRecord> records = CSVFormat.RFC4180.builder().setHeader().setSkipHeaderRecord(true).build().parse(reader);
-
+            
+            //Loop through each record and create an Appointment object
             for (CSVRecord record : records)
             {
                 String appointmentID = record.get("Appointment ID");
@@ -38,6 +47,7 @@ public class AppointmentRepository implements CsvRepository<Appointment>
                 String timeslot = record.get("Timeslot");
                 String status = record.get("Appointment_Status");
 
+                // Create a new appointment object
                 Appointment appointment = new Appointment(appointmentID, patientID, doctorID, date, timeslot, status);
                 appointmentArrayList.add(appointment);
             }
@@ -65,11 +75,14 @@ public class AppointmentRepository implements CsvRepository<Appointment>
     {
         try
         {
+            // Create a writer for the CSV file
             Writer writer = new FileWriter(CSV_FILE_PATH_APPOINTMENT);
             CSVFormat csvFormat = CSVFormat.DEFAULT.builder().setHeader(APPOINTMENT_HEADERS).build();
 
+            // Create a CSVPrinter to write data
             CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat);
 
+             // Loop through each appointment and write its data to the CSV
             for (Appointment appointment : appointmentList)
             {
                 csvPrinter.printRecord(appointment.getAppointmentId(), appointment.getPatientId(), appointment.getDoctorId(), appointment.getDate(), appointment.getTimeslot(), appointment.getStatus());
