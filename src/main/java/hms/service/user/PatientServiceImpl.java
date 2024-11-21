f41af3021f29bbac366f2bf39613ded7259192cd
Xhryo <oyongxiang@gmail.com>
2024-11-21 12:55:46 +0800

    csv and pw hiding

--- src/main/java/hms/service/user/PatientServiceImpl.java
+++ src/main/java/hms/service/user/PatientServiceImpl.java
@@ -2,6 +2,8 @@ package hms.service.user;
 
 import java.util.List;
 import java.util.Scanner;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 import hms.model.appointment.Appointment;
 import hms.model.appointment.AppointmentManager;
@@ -39,6 +41,11 @@ public class PatientServiceImpl extends UserService
      */
     public void printMenu()
     {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatDateTime = dateTime.format(myFormatObj);
+        System.out.println("Welcome to HMS Hospital "+ authenticatedPatient.getName() + "!");
+        System.out.println("Today is " + formatDateTime);
         System.out.print("""
                                  ========== Patient's Menu ==========
                                  (1) View Medical Record
@@ -50,7 +57,6 @@ public class PatientServiceImpl extends UserService
                                  (7) View Scheduled Appointments
                                  (8) View Past Appointment Outcome Records
                                  (0) Logout
-                                 (11) (DEV) Add diagnose
                                  """);
         System.out.print("Select an option: ");
     }
